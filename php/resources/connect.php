0e11f14597c9bce2eee6d7d381c1b4e124a01484
DrSloughKeg <bpye1995@gmail.com>
2023-10-18 19:46:18 -0400

    added connect.php, attempted to pull data from db into index

---
+++ php/resources/connect.php
@@ -0,0 +1,16 @@
+<?php
+
+//variables
+$type = "mysql";
+$host = "localhost";
+$dbName = "fsd10_Uniform";
+$port = "8889"; //Benjamins port: 8889
+$charset = "utf8";
+$dbUsername = "fsduser";
+$dbPassword = "myDBpw";
+
+//connection string (data source name)
+$DSN = "{$type}:host={$host};dbname={$dbName};port={$port};charset={$charset}";
+
+//open new database connection
+$db = new PDO($DSN, $dbUsername, $dbPassword);

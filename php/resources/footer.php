c64a15da8d455ea20dccb7e5a5683e76c8f29a11
DrSloughKeg <bpye1995@gmail.com>
2023-10-27 14:23:56 -0400

    why did the footer have a head? and scrum notes.

--- php/resources/footer.php
+++ php/resources/footer.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="css/styleHome.css" />
-      <link
-         rel="stylesheet"
-         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
-      <title>JustSell</title>
-
-      <!-- Favicons -->
-      <link href="./images/small_icon.png" rel="icon">
-  </head>
-
 <footer class="footer">
 
   <section class="flex">

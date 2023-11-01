d04a452e0e1aacd392c83bf9ea160d27f22cf37c
Dripamine <dripamine@gmail.com>
2023-11-01 11:49:18 -0400

    store username input value to pre-fill

--- php/login.php
+++ php/login.php
@@ -1,18 +1,22 @@
 <?php
 require "resources/connect.php";
 
-if ($IsLogIn) {
+if ($IsLogIn){
   // Redirect
   header("location: index.php");
   die();
 }
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
+$usernameInput = ""; // Initialize the username input variable
+
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
   $errorMsgs = array(); // Create an array for error messages
 
   // Add validation functions to functions.php
   if (IsEmpty($_POST, 'username')) {
     $errorMsgs['username'] = "<p style='color: red; text-align: center;'>Username is required.</p>";
+  } else {
+    $usernameInput = $_POST['username']; // Store the username input when it's not empty
   }
 
   if (IsEmpty($_POST, 'password')) {
@@ -26,9 +30,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $query->execute(["username" => $_POST['username']]);
     $data = $query->fetch();
 
-    if ($data != NULL) {
+    if ($data != NULL){
       // If the username exists, verify the password
-      if (password_verify($_POST['password'], $data['Password'])) {
+      if (password_verify($_POST['password'], $data['Password'])){
         // TBD: Add log here
         // Set the "user_id" session variable with the user's ID
         $_SESSION['login'] = true;
@@ -54,7 +58,7 @@ include "resources/header.php";
     <form action="login.php" method="POST">
       <div class="box">
         <p>Username: </p>
-        <input class="input" id="username" name="username" type="text" />
+        <input class="input" id="username" name="username" type="text" value="<?= $usernameInput; ?>" />
         <?= $errorMsgs['username'] ?? ''; ?> <!-- Display username error near the input -->
       </div>
       <div class="box">
@@ -62,9 +66,8 @@ include "resources/header.php";
         <input class="input" id="password" name="password" type="password" />
         <?= $errorMsgs['password'] ?? ''; ?> <!-- Display password error near the input -->
       </div>
-      <br>
       <h3>Don't have an account? <a href="registration.php"> <br> Register now!</a> </h3>
-      <input class="btn" type="submit" value="Log in!" />
+      <input class="btn" type="submit" value="Log in!" /> 
     </form>
   </section>
 </div>

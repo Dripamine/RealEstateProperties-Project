2a0d9a3aefeba00facb9b19e17282958be21d712
DrSloughKeg <bpye1995@gmail.com>
2023-11-01 10:57:03 -0400

    Admin panel template finished

--- php/admin.php
+++ php/admin.php
@@ -15,14 +15,116 @@ if ($IsLogIn){
 	$query->execute(["id"=> $_SESSION['user_id']]);
   $data = $query->fetch();
 
+
   if ($data['Permission'] == 1){
     echo "You are unauthorized to access this panel.";
   }
   if ($data['Permission'] == 2){
     echo "Welcome to the agent panel.";
-  }
-  if ($data['Permission'] == 3){
+    //fetch Agent ID
+    $sql = "SELECT AgentID FROM agents WHERE LoginID = :id";
+    $query = $db->prepare($sql);
+    $query->execute(["id"=> $_SESSION['user_id']]);
+    $data = $query->fetch();
+    $agentID = $data['AgentID'];
+
+    //fetch all agent listings
+    $sql = "SELECT * FROM properties WHERE AgentID = :id";
+    $query = $db->prepare($sql);
+    $query->execute(["id"=> $agentID]);
+    
+    ?>
+    <div>
+      <section>
+        <?php while($data = $query->fetch()) : ?>
+        <div class="box">
+          <p>Property Number: <?=$data['PropertyID'];?> </p>
+          <p>Address: <?=$data['StreetNum'];?>, <?=$data['StreetName'];?>, <?=$data['Postal'];?></p>
+          <p>Price: <?=$data['Price'];?></p>
+          <p>Edit / Delete</p>
+        </div>
+          <? endwhile?>
+      </section>
+      <section>
+        <?php
+        //fetch all offers
+        $sql = "SELECT o.*, p.AgentID FROM `propertyoffers` o JOIN `properties` p ON o.PropertyID = p.PropertyID WHERE AgentID = :id";
+        $query = $db->prepare($sql);
+        $query->execute(["id"=> $agentID]);
+
+        while($data = $query->fetch()) : ?>
+          <div class="box">
+            <p>Offer for Property Number: <?=$data['PropertyID'];?></p>
+            <p>Amount: <?=$data['OfferAmount'];?></p>
+            <p>Status:<?=$data['OfferStatus'];?></p>
+            <p>Edit / Delete</p>
+          </div>
+        <? endwhile?>
+      </section>
+
+    </div>
+    <?php
+  } else if ($data['Permission'] == 3){
     echo "Welcome to the admin panel";
+    //fetch adminID -wait, i dont need to do this
+
+    /*$sql = "SELECT AdminID FROM admins WHERE LoginID = :id";
+    $query = $db->prepare($sql);
+    $query->execute(["id"=> $_SESSION['user_id']]);
+    $data = $query->fetch();
+    $adminID = $data['AgentID'];
+    */
+
+    //fetch all  listings
+    $sql = "SELECT * FROM properties";
+    $query = $db->prepare($sql);
+    $query->execute();
+    
+    ?>
+    <div>
+      <section>
+        <?php while($data = $query->fetch()) : ?>
+        <div class="box">
+          <p>Property Number: <?=$data['PropertyID'];?> </p>
+          <p>Address: <?=$data['StreetNum'];?>, <?=$data['StreetName'];?>, <?=$data['Postal'];?></p>
+          <p>Price: <?=$data['Price'];?></p>
+          <p>Edit / Delete</p>
+        </div>
+          <? endwhile?>
+      </section>
+      <section>
+        <?php
+        //fetch all offers
+        $sql = "SELECT * FROM propertyoffers";
+        $query = $db->prepare($sql);
+        $query->execute();
+
+        while($data = $query->fetch()) : ?>
+          <div class="box">
+            <p>Offer for Property Number: <?=$data['PropertyID'];?></p>
+            <p>Amount: <?=$data['OfferAmount'];?></p>
+            <p>Status:<?=$data['OfferStatus'];?></p>
+            <p>Edit / Delete</p>
+          </div>
+        <? endwhile?>
+      </section>
+      <section>
+        <?php
+        //fetch all users
+        $sql = "SELECT * FROM users";
+        $query = $db->prepare($sql);
+        $query->execute();
+
+        while($data = $query->fetch()) : ?>
+          <div class="box">
+            <p>User: <?=$data['FirstName'];?> <?=$data['LastName'];?></p>
+            <p>Email: <?=$data['Email'];?></p>
+            <p>Postal:<?=$data['Postal'];?></p>
+            <p>Edit / Delete</p>
+          </div>
+        <? endwhile?>
+      </section>
+    <?php
   }
 }
 

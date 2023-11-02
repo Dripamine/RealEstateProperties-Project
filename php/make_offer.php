729f2a5b05d8e6b796fa1a3fdd07585d1552b2fc
KarinaDeVargas <134572496+KarinaDeVargas@users.noreply.github.com>
2023-11-02 16:57:55 -0300

    Minor changes part 2 - make offer php

--- php/make_offer.php
+++ php/make_offer.php
@@ -3,46 +3,57 @@ require "resources/connect.php";
 include "resources/header.php";
 ?>
 
+<?php
+//verify if the user is logged in to send the offer
+
+?>
+
+
 <style>
   #confirmation {
     display: none;
   }
 </style>
 
-<!-- search filter section starts  -->
-
+<!-- send offer section starts  -->
 <div class="home">
-   <section class="filters" style="padding-bottom: 70px;">
-      <form action="" method="post">
-         <div id="close-filter"><i class="fas fa-times"></i></div>
-         <h3>Send Offer</h3>
-         <?php
-            $sql_selected_property = $db->prepare("SELECT * FROM `properties` WHERE PropertyID = 1");
-            if ($sql_selected_property->execute()) {
-               $selected_property = $sql_selected_property->fetch(PDO::FETCH_ASSOC);
-            } else {
-               $errorInfo = $sql_selected_property->errorInfo();
-               echo 'Error: ' . $errorInfo[2];
-            }
-         ?>
-         <div class="flex">
-            <div class="box" style="padding: 25px 0 0 25px; display: block;">
-               <p name="lblPropertyid" style="font-weight: bold;">PropertyId: <?= $selected_property['PropertyID'] ?></p>
-            </div>
-            <div class="box" style="padding: 15px 0 0 25px; display: block;">
-               <p name="lblAskingprice" style="font-weight: bold;">Asking Price: $<?= number_format($selected_property['Price'], 2, '.', ',') ?></p>
-            </div>
-            <div class="box" style="padding: 25px; display: block;">
-               <p style="font-weight: bold;">Your offer</p>
-               <input type="text" name="txtOffer" required placeholder="0.00" class="input" style="width: 200px;">
-            </div>
+   <section class="center">
+      <form action="" method="POST">
+         
+         <div class="box">
+            <h3>Send Offer</h3>
+            <?php
+               $sql_selected_property = $db->prepare("SELECT * FROM `properties` WHERE PropertyID = 1");
+               if ($sql_selected_property->execute()) {
+                  $selected_property = $sql_selected_property->fetch(PDO::FETCH_ASSOC);
+               } else {
+                  $errorInfo = $sql_selected_property->errorInfo();
+                  echo 'Error: ' . $errorInfo[2];
+               }
+            ?>
          </div>
-         <input type="submit" value="Submit offer" name="btnSubmitoffer" class="btn" style="width: 400px; margin: 0 auto;" id="submitOfferButton">
+
+         <div class="box">
+            <p name="lblPropertyid">Property ID:  <?= $selected_property['PropertyID'] ?></p>
+         </div>
+
+         <div class="box">
+            <p name="lblAskingprice"><br>Asking Price:  $<?= number_format($selected_property['Price'], 2, '.', ',') ?></p>
+         </div>
+
+         <div class="box">
+            <p style="font-weight: bold;"><br>Your offer:</p>
+            <input type="text" name="txtOffer" required placeholder="0.00" class="input">
+         </div>        
+
+         <input type="submit" value="Submit offer" name="btnSubmitoffer" class="btn" id="submitOfferButton">
       </form>
    </section>
 </div>
+<!-- send offer section ends  -->
 
 
+<!-- confirmation message - I would like to change it to show up as a pop-up -->
 <section id="confirmation" class="filters" style="padding-bottom: 200px; text-align: center;">
    <form style="width: 900px; margin: 0 auto;">
       <div class="flex">
@@ -63,12 +74,9 @@ include "resources/header.php";
       </div>
    </form>
 </section>
-
 </body>
 </html>
 
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
 <!-- JavaScript to handle the form submission and display the confirmation section -->
 <script>
